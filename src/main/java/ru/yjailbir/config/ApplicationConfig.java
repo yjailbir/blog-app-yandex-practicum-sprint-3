f292fe7a581e52d5e8bbd0a1963423a0d6071a47
yjailbir <nkudashko@mail.ru>
2025-05-10 21:05:24 +0300

    config update

--- src/main/java/ru/yjailbir/config/ApplicationConfig.java
+++ src/main/java/ru/yjailbir/config/ApplicationConfig.java
@@ -1,8 +1,12 @@
 package ru.yjailbir.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -10,9 +14,12 @@ import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "ru.yjailbir")
+@PropertySource("classpath:application.properties")
 public class ApplicationConfig {
     @Bean
     public ITemplateResolver templateResolver() {
@@ -39,4 +46,23 @@ public class ApplicationConfig {
         return resolver;
     }
 
+    @Bean
+    public DataSource dataSource(
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.username}") String username,
+            @Value("${spring.datasource.password}") String password
+    ) {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }
